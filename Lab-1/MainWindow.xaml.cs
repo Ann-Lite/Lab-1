42f9e933deaa10c1aa2e46b0fc4accd267c468d8
аня сергиенко <annsergienko15@gmail.com>
2021-10-28 22:15:40 +0300

    Добавьте файлы проекта.

---
+++ Lab-1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_4;
+
+namespace Lab_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Вычислить_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(count.Text, out int counts) == true)
+            {
+                Practice.MiniNumber(counts, out int miniNumber, out string number);
+                otvet.Text = miniNumber.ToString(); otvet2.Text = number;
+            }
+            else MessageBox.Show("Введены некорректные данные");
+        }
+
+        private void О_программе_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Найти минимум из n целых случайных чисел X, распределенных в диапазоне от 10 до 40." +
+                "Вывести на экран на одной строке сгенерированные числа, на другой строке результат");
+
+        }
+
+        private void Выход_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
